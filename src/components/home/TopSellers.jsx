5e10ddcfd28b2e917c8ef4c6d94a63dd08236235
Chris <chrislanzas89@outlook.com>
2025-08-19 18:35:42 -0400

    Made sellers dynamic and added loading states

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+  const [sellers, topSellers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const skeletonCount = sellers.length || 12;
+
+  async function getSellers() {
+    setLoading(true);
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    topSellers(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -13,26 +31,67 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading ? (
+                Array.from({ length: skeletonCount }).map((_, i) => (
+                  <li key={i}>
+                    <div className="author_list_pp">
+                      <div
+                        className="skeleton-box"
+                        style={{
+                          width: "50px",
+                          height: "50px",
+                          borderRadius: "50%",
+                        }}
+                      ></div>
+                    </div>
+                    <div className="author_list_info">
+                      <div
+                        className="skeleton-box"
+                        style={{
+                          width: "100px",
+                          height: "20px",
+                        }}
+                      ></div>
+                    </div>
+
+                    <span className="author_list_info">
+                      {" "}
+                      <div
+                        className="skeleton-box"
+                        style={{
+                          width: "40px",
+                          height: "20px",
+                        }}
+                      ></div>
+                    </span>
+                  </li>
+                ))
+              ) : (
+                <>
+                  {sellers.map((seller, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Link to="/author">
+                          <img
+                            className="lazy pp-author"
+                            src={seller.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to="/author">{seller.authorName}</Link>
+                        <span>{seller.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
+                  
+                </>
+              )}
             </ol>
           </div>
         </div>
@@ -40,5 +99,4 @@ const TopSellers = () => {
     </section>
   );
 };
-
 export default TopSellers;
