178229e8465a7baa17a8c6e866e75de9c23964bd
Chris <chrislanzas89@outlook.com>
2025-08-22 12:31:22 -0400

    Made dynamic links to author page and author items fetched dynamically with loading state

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+  const [loading, setLoading] = useState();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const { id } = useParams();
+  const [author, setAuthor] = useState();
+
+  async function getAuthor(id) {
+    setLoading(true);
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+    );
+    setAuthor(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getAuthor(id);
+  }, [id]);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -21,43 +43,101 @@ const Author = () => {
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
-
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+              {loading ? (
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <Skeleton
+                          width="150px"
+                          height="150px"
+                          borderRadius="50%"
+                        />
+                        <div className="profile_name">
+                          <h4>
+                            <Skeleton width="200px" />
+                            <span className="profile_username">
+                              <Skeleton width="100px" />
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              <Skeleton width="250px" />
+                            </span>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          <Skeleton width="150px" height="40px" />
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                  <div className="de_tab tab_simple">
+                    <div className="de_tab_content">
+                      <div className="tab-1">
+                        <div className="row">
+                          {Array.from({ length: 8 }).map((_, i) => (
+                            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                              <Skeleton width="100%" height="400px" />
+                            </div>
+                          ))}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              ) : (
+                <>
+                  <div className="col-md-12">
+                    <div className="d_profile de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_avatar">
+                          <div className="img-wrapper">
+                            <img src={author?.authorImage} alt="" />
 
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
-                </div>
-              </div>
+                            <i className="fa fa-check"></i>
+                          </div>
+                          <div className="profile_name">
+                            <h4>
+                              {author?.authorName}
+                              <span className="profile_username">
+                                @{author?.tag}
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                {author?.address}
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="profile_follow de-flex">
+                        <div className="de-flex-col">
+                          <div className="profile_follower">
+                            {author?.followers} followers
+                          </div>
+                          <Link to="#" className="btn-main">
+                            Follow
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <div className="de_tab tab_simple">
+                      <AuthorItems
+                        items={author?.nftCollection || []}
+                        loading={loading}
+                        authorImage={author?.authorImage}
+                      />
+                    </div>
+                  </div>{" "}
+                </>
+              )}
             </div>
           </div>
         </section>
